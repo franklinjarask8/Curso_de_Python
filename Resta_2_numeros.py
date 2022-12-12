cd7b8e14e27aab260f87eeadb6e7348f71770f15
franklinjarask8 <frankjara07@gmail.com>
2022-12-12 15:13:18 +0100

    se arreglan los códigos de resta y de suma
    se entra en los 2 archivos para arreglar el código

--- Resta_2_numeros.py
+++ Resta_2_numeros.py
@@ -1,4 +1,4 @@
 a=10
 b=3
 resta=a-b
-print ('el resultado de la resta es ' + str(resta))
+print ('el resultado de la resta es ' + str(resta)) #se comprueba que el código funciona correctamente
--- Suma_2_numeros.py
+++ Suma_2_numeros.py
@@ -1,1 +1,4 @@
-print ('Hola mundo')
+a= 5
+b= 6
+suma= a+b
+print ('El resultado de la suma es '+ str(suma)) # la función str es para transformar el número en cadena 'string'
